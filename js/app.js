8bcd8610fa2835ee192dfde6958b652c1f3476dd
FRESCH-Branding <info@freschbranding.nl>
2022-07-10 10:48:22 +0200

    add js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+//website title changes
+
 window.onload = function () {
   var pageTitle = document.title;
   var attentionMessage = "HI!👋 kan ik iets voor je doen?";
@@ -12,3 +14,29 @@ window.onload = function () {
     }
   });
 };
+
+// navigation animation
+
+const navigationSlide = () => {
+  const burger = document.querySelector(".Burger-menu");
+  const nav = document.querySelector(".Nav-links");
+  const navLinks = document.querySelectorAll(".Nav-links li");
+
+  //toggle nav
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    //links animation
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `Nav-links-fade 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`;
+      }
+    });
+  });
+};
+
+navigationSlide();
