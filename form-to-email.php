9642587b836fd8567ef2af0cc89ac7057d2f8454
FRESCH-Branding <info@freschbranding.nl>
2023-04-19 14:49:58 +0200

    add php mailer

---
+++ form-to-email.php
@@ -0,0 +1,52 @@
+<?php
+
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.mijndomein.nl';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'info@freschbranding.nl';                     //SMTP username
+    $mail->Password   = '*Freschbranding98!';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('info@freschbranding.nl', $_POST['naam']);
+    $mail->addAddress('info@freschbranding.nl');     //Add a recipient
+
+    //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = $_POST['onderwerp'];
+    $mail->Body    = 
+        $_POST['naam'].'<br>'.
+        $_POST['telefoonnummer'].'<br>'.
+        $_POST['email'].'<br>'.
+        '<br><br>'.
+        $_POST['bericht'];
+    
+    $mail->AltBody = $_POST['bericht'];
+
+    $mail->send();
+
+    header('Location: contact.html');
+    
+} catch (Exception $e) {
+    echo "Bericht kon niet worden verzonden. Mailer Error: {$mail->ErrorInfo}";
+}
+?>
